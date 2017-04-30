bdea4749fd255ac411803b64f6bc75b15aad94d9
smmfirouz <mahdiar67@gmail.com>
2017-04-30 20:28:59 +0430

    setter getter

--- src/model/ServerInfo.java
+++ src/model/ServerInfo.java
@@ -1,5 +1,19 @@
 package model;
 
 public class ServerInfo {
+private int port;
+private int ip;
+public int getPort() {
+	return port;
+}
+public void setPort(int port) {
+	this.port = port;
+}
+public int getIp() {
+	return ip;
+}
+public void setIp(int ip) {
+	this.ip = ip;
+}
 
 }
